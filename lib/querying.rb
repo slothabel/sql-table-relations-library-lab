6cf6075d86e17836ed88b99a42869198e91bf342
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-15 20:18:36 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,72 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+  FROM books 
+  INNER JOIN series
+  ON series.id = books.series_id
+  WHERE series.id = 1
+  ORDER BY books.year;"
 end
 
+
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto
+  FROM characters 
+  ORDER BY characters.motto  
+  LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species) 
+  DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  INNER JOIN series
+  ON authors.id = series.author_id
+  INNER JOIN subgenres
+  ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  INNER JOIN subgenres
+  ON subgenres.id = series.subgenre_id
+  INNER JOIN books
+  ON series.id = books.series_id
+  INNER JOIN character_books
+  ON character_books.book_id = books.id
+  INNER JOIN characters
+  ON characters.id = character_books.character_id
+  WHERE characters.species = 'human'
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "  SELECT characters.name, COUNT(character_books.book_id) 
+  AS book_count 
+  FROM characters 
+  INNER JOIN character_books
+  ON character_books.character_id = characters.id
+  GROUP BY characters.name 
+  ORDER BY book_count DESC, characters.name ;"
 end
+
+
+# series.id = books.series_id
+# books.id = character_books.book_id
+# authors.id = series.author_id
+# subgenres.id = series.subgenre_id
+
+# characters.species = "human" 
+
+
+
+
+ 
